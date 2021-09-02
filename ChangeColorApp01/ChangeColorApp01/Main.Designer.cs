ba9d410a3e599cb9f1fec6ac56c59306ab8cfb69
jaekook <jaekook4849@gmail.com>
2021-09-02 15:30:26 +0900

    Change Application Color

---
+++ ChangeColorApp01/ChangeColorApp01/Main.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace ChangeColorApp01
+{
+    partial class ChangeColorAppForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundColorButton = new System.Windows.Forms.Button();
+            this.foregroundColorButton = new System.Windows.Forms.Button();
+            this.backgroundColorDialog = new System.Windows.Forms.ColorDialog();
+            this.foregroundColorDialog = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // backgroundColorButton
+            // 
+            this.backgroundColorButton.Location = new System.Drawing.Point(100, 50);
+            this.backgroundColorButton.Name = "backgroundColorButton";
+            this.backgroundColorButton.Size = new System.Drawing.Size(180, 55);
+            this.backgroundColorButton.TabIndex = 0;
+            this.backgroundColorButton.Text = "배경색";
+            this.backgroundColorButton.UseVisualStyleBackColor = true;
+            // 
+            // foregroundColorButton
+            // 
+            this.foregroundColorButton.Location = new System.Drawing.Point(100, 150);
+            this.foregroundColorButton.Name = "foregroundColorButton";
+            this.foregroundColorButton.Size = new System.Drawing.Size(180, 55);
+            this.foregroundColorButton.TabIndex = 1;
+            this.foregroundColorButton.Text = "전경색";
+            this.foregroundColorButton.UseVisualStyleBackColor = true;
+            // 
+            // ChangeColorAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.foregroundColorButton);
+            this.Controls.Add(this.backgroundColorButton);
+            this.Name = "ChangeColorAppForm";
+            this.Text = "Change Color";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backgroundColorButton;
+        private System.Windows.Forms.Button foregroundColorButton;
+        private System.Windows.Forms.ColorDialog backgroundColorDialog;
+        private System.Windows.Forms.ColorDialog foregroundColorDialog;
+    }
+}
+
---
+++ ChangeColorApp01/ChangeColorApp01/Main.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeColorApp01
+{
+    public partial class ChangeColorAppForm : Form
+    {
+        #region 생성자 - ChangeColorAppForm()
+
+        public ChangeColorAppForm()
+        {
+            InitializeComponent();
+
+            #region 이벤트를 설정한다.
+
+            Load += Form_Load;
+            this.backgroundColorButton.Click += backgroundColorButton_Click;
+            this.foregroundColorButton.Click += foregroundColorButton_Click;
+
+            #endregion
+        }
+
+        #endregion
+
+        #region 폼 로드 시 처리하기 - Form_Load(sender, e)
+
+        /// <summary>
+        /// 폼 로드시 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void Form_Load(object sender, EventArgs e)
+        {
+            int[] backgroundColorArray =
+            {
+                0xffffff, 0xffc0c0, 0xffe0c0, 0xffffc0, 0xc0ffc0,
+                0xc0ffff, 0xc0c0ff, 0xffc0ff, 0xe0e0e0, 0xff8080,
+                0xffc080, 0xffff80, 0x80ff80, 0x80ffff, 0x8080ff,
+                0xff80ff
+            };
+
+            this.backgroundColorDialog.CustomColors = backgroundColorArray;
+
+            int[] foregroundColorArray =
+            {
+                0x808080, 0xff0000, 0xff8000, 0xffff00, 0x00ff00,
+                0x00ffff, 0x0000ff, 0xff00ff, 0x000000, 0xc00000,
+                0x804000, 0xc0c000, 0x008000, 0x00c0c0, 0x0000c0,
+                0x800080
+            };
+
+            this.foregroundColorDialog.CustomColors = foregroundColorArray;
+        }
+
+        #endregion
+        #region 전경색 버튼 클릭 시 처리하기 - foregroundColorButton_Click(sender, e)
+
+        /// <summary>
+        /// 전경색 버튼 클릭시 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void foregroundColorButton_Click(object sender, EventArgs e)
+        {
+            this.foregroundColorDialog.Color = this.ForeColor;
+
+            if (this.foregroundColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                ForeColor = this.foregroundColorDialog.Color;
+            }
+        }
+
+        #endregion
+        #region 배경색 버튼 클릭 시 처리하기 - backgroundColorButton_Click(sender, e)
+
+        /// <summary>
+        /// 배경색 버튼 클릭시 처리하기
+        /// </summary>
+        /// <param name="sender">이벤트 발생자</param>
+        /// <param name="e">이벤트 인자</param>
+        private void backgroundColorButton_Click(object sender, EventArgs e)
+        {
+            this.backgroundColorDialog.Color = this.BackColor;
+
+            if (this.backgroundColorDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.BackColor = this.backgroundColorDialog.Color;
+
+                this.backgroundColorButton.BackColor = this.backgroundColorDialog.Color;
+                this.foregroundColorButton.BackColor = this.backgroundColorDialog.Color;
+            }
+        }
+
+        #endregion
+    }
+}
