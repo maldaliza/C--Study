4550767d1ca7956806c1239bab407b85c99cc392
jaekook <jaekook4849@gmail.com>
2021-09-28 17:18:40 +0900

    Add Dynamic Button Creation

--- DBConnectionApp01/DBConnectionApp01/Form1.Designer.cs
+++ DBConnectionApp01/DBConnectionApp01/Form1.Designer.cs
@@ -42,11 +42,18 @@ namespace DBConnectionApp01
             this.buttonPanel = new System.Windows.Forms.TableLayoutPanel();
             this.insertButton = new System.Windows.Forms.Button();
             this.searchButton = new System.Windows.Forms.Button();
+            this.bottomPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.totalPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.pagingPanel = new System.Windows.Forms.Panel();
             this.totalDivision.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.topPanel.SuspendLayout();
             this.inputDataPanel.SuspendLayout();
             this.buttonPanel.SuspendLayout();
+            this.bottomPanel.SuspendLayout();
+            this.totalPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // totalDivision
@@ -55,6 +62,7 @@ namespace DBConnectionApp01
             this.totalDivision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.totalDivision.Controls.Add(this.dataGridView, 0, 1);
             this.totalDivision.Controls.Add(this.topPanel, 0, 0);
+            this.totalDivision.Controls.Add(this.bottomPanel, 0, 2);
             this.totalDivision.Dock = System.Windows.Forms.DockStyle.Fill;
             this.totalDivision.Location = new System.Drawing.Point(0, 0);
             this.totalDivision.Margin = new System.Windows.Forms.Padding(0);
@@ -63,7 +71,7 @@ namespace DBConnectionApp01
             this.totalDivision.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.totalDivision.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.totalDivision.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.totalDivision.Size = new System.Drawing.Size(384, 461);
+            this.totalDivision.Size = new System.Drawing.Size(584, 461);
             this.totalDivision.TabIndex = 0;
             // 
             // dataGridView
@@ -80,7 +88,7 @@ namespace DBConnectionApp01
             this.dataGridView.RowHeadersVisible = false;
             this.dataGridView.RowTemplate.Height = 23;
             this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView.Size = new System.Drawing.Size(384, 276);
+            this.dataGridView.Size = new System.Drawing.Size(584, 276);
             this.dataGridView.TabIndex = 0;
             // 
             // topPanel
@@ -96,7 +104,7 @@ namespace DBConnectionApp01
             this.topPanel.RowCount = 2;
             this.topPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 75F));
             this.topPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.topPanel.Size = new System.Drawing.Size(384, 138);
+            this.topPanel.Size = new System.Drawing.Size(584, 138);
             this.topPanel.TabIndex = 1;
             // 
             // inputDataPanel
@@ -119,27 +127,27 @@ namespace DBConnectionApp01
             this.inputDataPanel.RowCount = 2;
             this.inputDataPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.inputDataPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.inputDataPanel.Size = new System.Drawing.Size(384, 103);
+            this.inputDataPanel.Size = new System.Drawing.Size(584, 103);
             this.inputDataPanel.TabIndex = 0;
             // 
             // phoneNumberTextBox
             // 
             this.phoneNumberTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.phoneNumberTextBox.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.phoneNumberTextBox.Location = new System.Drawing.Point(57, 66);
+            this.phoneNumberTextBox.Location = new System.Drawing.Point(87, 66);
             this.phoneNumberTextBox.Margin = new System.Windows.Forms.Padding(0, 15, 10, 0);
             this.phoneNumberTextBox.Name = "phoneNumberTextBox";
-            this.phoneNumberTextBox.Size = new System.Drawing.Size(124, 23);
+            this.phoneNumberTextBox.Size = new System.Drawing.Size(194, 23);
             this.phoneNumberTextBox.TabIndex = 5;
             // 
             // addressTextBox
             // 
             this.addressTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.addressTextBox.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.addressTextBox.Location = new System.Drawing.Point(248, 15);
+            this.addressTextBox.Location = new System.Drawing.Point(378, 15);
             this.addressTextBox.Margin = new System.Windows.Forms.Padding(0, 15, 10, 0);
             this.addressTextBox.Name = "addressTextBox";
-            this.addressTextBox.Size = new System.Drawing.Size(126, 23);
+            this.addressTextBox.Size = new System.Drawing.Size(196, 23);
             this.addressTextBox.TabIndex = 4;
             // 
             // addressLabel
@@ -147,10 +155,10 @@ namespace DBConnectionApp01
             this.addressLabel.AutoSize = true;
             this.addressLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.addressLabel.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.addressLabel.Location = new System.Drawing.Point(191, 0);
+            this.addressLabel.Location = new System.Drawing.Point(291, 0);
             this.addressLabel.Margin = new System.Windows.Forms.Padding(0);
             this.addressLabel.Name = "addressLabel";
-            this.addressLabel.Size = new System.Drawing.Size(57, 51);
+            this.addressLabel.Size = new System.Drawing.Size(87, 51);
             this.addressLabel.TabIndex = 2;
             this.addressLabel.Text = "주소";
             this.addressLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -163,7 +171,7 @@ namespace DBConnectionApp01
             this.phoneNumberLabel.Location = new System.Drawing.Point(0, 51);
             this.phoneNumberLabel.Margin = new System.Windows.Forms.Padding(0);
             this.phoneNumberLabel.Name = "phoneNumberLabel";
-            this.phoneNumberLabel.Size = new System.Drawing.Size(57, 52);
+            this.phoneNumberLabel.Size = new System.Drawing.Size(87, 52);
             this.phoneNumberLabel.TabIndex = 1;
             this.phoneNumberLabel.Text = "전화번호";
             this.phoneNumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -176,7 +184,7 @@ namespace DBConnectionApp01
             this.nameLabel.Location = new System.Drawing.Point(0, 0);
             this.nameLabel.Margin = new System.Windows.Forms.Padding(0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(57, 51);
+            this.nameLabel.Size = new System.Drawing.Size(87, 51);
             this.nameLabel.TabIndex = 0;
             this.nameLabel.Text = "이름";
             this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -185,10 +193,10 @@ namespace DBConnectionApp01
             // 
             this.nameTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.nameTextBox.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.nameTextBox.Location = new System.Drawing.Point(57, 15);
+            this.nameTextBox.Location = new System.Drawing.Point(87, 15);
             this.nameTextBox.Margin = new System.Windows.Forms.Padding(0, 15, 10, 0);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(124, 23);
+            this.nameTextBox.Size = new System.Drawing.Size(194, 23);
             this.nameTextBox.TabIndex = 3;
             // 
             // buttonPanel
@@ -208,7 +216,7 @@ namespace DBConnectionApp01
             this.buttonPanel.Name = "buttonPanel";
             this.buttonPanel.RowCount = 1;
             this.buttonPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.buttonPanel.Size = new System.Drawing.Size(384, 35);
+            this.buttonPanel.Size = new System.Drawing.Size(584, 35);
             this.buttonPanel.TabIndex = 1;
             // 
             // insertButton
@@ -218,7 +226,7 @@ namespace DBConnectionApp01
             this.insertButton.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.insertButton.Location = new System.Drawing.Point(3, 3);
             this.insertButton.Name = "insertButton";
-            this.insertButton.Size = new System.Drawing.Size(57, 29);
+            this.insertButton.Size = new System.Drawing.Size(91, 29);
             this.insertButton.TabIndex = 0;
             this.insertButton.Text = "추가";
             this.insertButton.UseVisualStyleBackColor = false;
@@ -229,19 +237,85 @@ namespace DBConnectionApp01
             this.searchButton.BackColor = System.Drawing.SystemColors.Control;
             this.searchButton.Dock = System.Windows.Forms.DockStyle.Fill;
             this.searchButton.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.searchButton.Location = new System.Drawing.Point(66, 3);
+            this.searchButton.Location = new System.Drawing.Point(100, 3);
             this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(57, 29);
+            this.searchButton.Size = new System.Drawing.Size(91, 29);
             this.searchButton.TabIndex = 1;
             this.searchButton.Text = "검색";
             this.searchButton.UseVisualStyleBackColor = false;
             this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
             // 
+            // bottomPanel
+            // 
+            this.bottomPanel.ColumnCount = 2;
+            this.bottomPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.bottomPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.bottomPanel.Controls.Add(this.totalPanel, 1, 0);
+            this.bottomPanel.Controls.Add(this.pagingPanel, 0, 0);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 414);
+            this.bottomPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.RowCount = 1;
+            this.bottomPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.bottomPanel.Size = new System.Drawing.Size(584, 47);
+            this.bottomPanel.TabIndex = 2;
+            // 
+            // totalPanel
+            // 
+            this.totalPanel.ColumnCount = 2;
+            this.totalPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.totalPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.totalPanel.Controls.Add(this.totalLabel, 0, 0);
+            this.totalPanel.Controls.Add(this.totalTextBox, 1, 0);
+            this.totalPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.totalPanel.Location = new System.Drawing.Point(408, 0);
+            this.totalPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.totalPanel.Name = "totalPanel";
+            this.totalPanel.RowCount = 1;
+            this.totalPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.totalPanel.Size = new System.Drawing.Size(176, 47);
+            this.totalPanel.TabIndex = 0;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.totalLabel.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.totalLabel.Location = new System.Drawing.Point(0, 0);
+            this.totalLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(70, 47);
+            this.totalLabel.TabIndex = 0;
+            this.totalLabel.Text = "TOTAL";
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.totalTextBox.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.totalTextBox.Location = new System.Drawing.Point(70, 10);
+            this.totalTextBox.Margin = new System.Windows.Forms.Padding(0, 10, 20, 0);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.ReadOnly = true;
+            this.totalTextBox.Size = new System.Drawing.Size(86, 25);
+            this.totalTextBox.TabIndex = 1;
+            this.totalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // pagingPanel
+            // 
+            this.pagingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pagingPanel.Location = new System.Drawing.Point(0, 0);
+            this.pagingPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.pagingPanel.Name = "pagingPanel";
+            this.pagingPanel.Size = new System.Drawing.Size(408, 47);
+            this.pagingPanel.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.ClientSize = new System.Drawing.Size(584, 461);
             this.Controls.Add(this.totalDivision);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -251,6 +325,9 @@ namespace DBConnectionApp01
             this.inputDataPanel.ResumeLayout(false);
             this.inputDataPanel.PerformLayout();
             this.buttonPanel.ResumeLayout(false);
+            this.bottomPanel.ResumeLayout(false);
+            this.totalPanel.ResumeLayout(false);
+            this.totalPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -270,6 +347,11 @@ namespace DBConnectionApp01
         private System.Windows.Forms.TableLayoutPanel buttonPanel;
         private System.Windows.Forms.Button insertButton;
         private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TableLayoutPanel bottomPanel;
+        private System.Windows.Forms.TableLayoutPanel totalPanel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.Panel pagingPanel;
     }
 }
 
--- DBConnectionApp01/DBConnectionApp01/Form1.cs
+++ DBConnectionApp01/DBConnectionApp01/Form1.cs
@@ -27,6 +27,42 @@ namespace DBConnectionApp01
             table.Columns.Add("전화번호", typeof(string));
             table.Columns.Add("주소", typeof(string));
 
+            // 행의 개수
+            Int64 count = db.CountDB();
+            totalTextBox.Text = Convert.ToString(count);
+
+
+            /**************************************
+             * 행의 개수로 offset을 지정하는 로직 *
+             **************************************/
+            int TotalPage = (int)count / 10 + 1;       // 1, 2, 3, ...
+            int offset = (int)count / 10;              // 0, 1, 2, ...
+
+            Control[] pageButton = new Control[7];
+            FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel();
+            
+            for (int i=0; i<TotalPage; i++)
+            {
+                pageButton[i] = new Button();
+                pageButton[i].Name = "pageButton" + (i+1).ToString();
+                pageButton[i].Size = new Size(40, 40);
+                pageButton[i].Text = (i+1).ToString();
+
+                pageButton[i].Width = 40;
+                pageButton[i].Height = 40;
+                pageButton[i].Margin = new Padding(10, 3, 3, 3);
+
+                pageButton[i].Click += new EventHandler(pageButton_Click);
+
+                flowLayoutPanel.Controls.Add(pageButton[i]);
+            }
+
+            pagingPanel.Controls.Add(flowLayoutPanel);
+            flowLayoutPanel.Dock = DockStyle.Fill;
+
+            /**************************************/
+
+
             // 데이터베이스 정보 조회
             db.SelectDB(table);
 
@@ -36,6 +72,7 @@ namespace DBConnectionApp01
             dataGridView.CurrentCell = dataGridView.Rows[dataGridView.RowCount - 1].Cells[0];
         }
 
+
         /*
          * 데이터 추가
          */
@@ -43,10 +80,31 @@ namespace DBConnectionApp01
         {
             db.InsertDB(table, nameTextBox.Text, phoneNumberTextBox.Text, addressTextBox.Text);
 
+            // 텍스트박스 초기화
+            nameTextBox.Text = "";
+            phoneNumberTextBox.Text = "";
+            addressTextBox.Text = "";
+
+            // 행의 개수
+            totalTextBox.Text = dataGridView.RowCount.ToString();
+
             // 마지막 행 포커싱
             dataGridView.CurrentCell = dataGridView.Rows[dataGridView.RowCount - 1].Cells[0];
         }
 
+
+        /*
+         * 페이징 버튼 클릭 이벤트
+         */
+        private void pageButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+
+        /*
+         * 검색기능 창 활성화
+         */
         private void searchButton_Click(object sender, EventArgs e)
         {
             SearchForm searchForm = new SearchForm();
